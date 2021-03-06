6049498f6b3c096b50f558448842d581bfdc2d30
Mark Tiddy <marktiddy@gmail.com>
2021-03-06 17:45:16 +0000

    Update [slug].js

--- pages/app/[slug].js
+++ pages/app/[slug].js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 import { appData } from '../../data.js';
 import Layout from '../../components/layout';
 import Link from 'next/link';
+import Head from 'next/head';
 
 const PrivacyPolicyPage = () => {
   const router = useRouter();
@@ -19,72 +20,78 @@ const PrivacyPolicyPage = () => {
   const { name, desc, apple, android, image, policy, alexa } = appToShow[0];
 
   return (
-    <Layout>
-      <Link href="/">
-        <a className={styles.backhome}>Home</a>
-      </Link>
-      <h1 className={styles.title}>{name}</h1>
-      <p>{desc}</p>
-      <div className={styles.privacyContainer}>
-        <div className={styles.privacyLeft}>
-          <img
-            src={image}
-            alt={`Screenshot of ${name}`}
-            style={styles.appImage}
-          />
-          <div className={styles.badges}>
-            {apple ? (
-              <div>
-                <a href={apple}>
-                  <img
-                    src="/images/appstorebadge.png"
-                    alt="Apple App Store Link"
-                    style={styles.downloadbadge}
-                  />
-                </a>
-              </div>
-            ) : (
-              ''
-            )}
-            {android ? (
-              <div>
-                {' '}
-                <a href={android}>
-                  <img
-                    src="/images/playbadge.png"
-                    alt="Google Play Store Link"
-                    style={styles.downloadbadge}
-                  />
-                </a>
-              </div>
-            ) : (
-              ''
-            )}
-            {alexa ? (
-              <div>
-                {' '}
-                <a href={alexa}>
-                  <img
-                    src="/images/skilllogo.png"
-                    alt="Alexa Skill Link"
-                    style={styles.downloadbadge}
-                  />
-                </a>
-              </div>
-            ) : (
-              ''
-            )}
+    <>
+      <Head>
+        <title>{name} - Apps by Mark Tiddy</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <Link href="/">
+          <a className={styles.backhome}>Home</a>
+        </Link>
+        <h1 className={styles.title}>{name}</h1>
+        <p>{desc}</p>
+        <div className={styles.privacyContainer}>
+          <div className={styles.privacyLeft}>
+            <img
+              src={image}
+              alt={`Screenshot of ${name}`}
+              style={styles.appImage}
+            />
+            <div className={styles.badges}>
+              {apple ? (
+                <div>
+                  <a href={apple}>
+                    <img
+                      src="/images/appstorebadge.png"
+                      alt="Apple App Store Link"
+                      style={styles.downloadbadge}
+                    />
+                  </a>
+                </div>
+              ) : (
+                ''
+              )}
+              {android ? (
+                <div>
+                  {' '}
+                  <a href={android}>
+                    <img
+                      src="/images/playbadge.png"
+                      alt="Google Play Store Link"
+                      style={styles.downloadbadge}
+                    />
+                  </a>
+                </div>
+              ) : (
+                ''
+              )}
+              {alexa ? (
+                <div>
+                  {' '}
+                  <a href={alexa}>
+                    <img
+                      src="/images/skilllogo.png"
+                      alt="Alexa Skill Link"
+                      style={styles.downloadbadge}
+                    />
+                  </a>
+                </div>
+              ) : (
+                ''
+              )}
+            </div>
+          </div>
+          <div className={styles.privacyRight}>
+            <h2>Privacy Policy</h2>
+            <div
+              dangerouslySetInnerHTML={{ __html: policy }}
+              className={styles.policyArea}
+            />
           </div>
         </div>
-        <div className={styles.privacyRight}>
-          <h2>Privacy Policy</h2>
-          <div
-            dangerouslySetInnerHTML={{ __html: policy }}
-            className={styles.policyArea}
-          />
-        </div>
-      </div>
-    </Layout>
+      </Layout>
+    </>
   );
 };
 
